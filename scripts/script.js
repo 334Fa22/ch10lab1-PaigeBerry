f4cca1aa9fa90f3b52ea964be2effcf633a10b8f
Paige Berry <s535282@nwmissouri.edu>
2022-09-20 13:09:43 -0500

    Berry Ch10 Lab

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+/*
+Name: Paige Berry
+File: script.js
+Date: September 20, 2022
+*/
+
+//Hamburger Menu Function
+function hamburger()
+  var menu = document.getElementById("menu-links");
+  if(menu.style.display ==="block"){
+    menu.style.display = none;
+  } else {
+    menu.style.display = "block";
+  }
